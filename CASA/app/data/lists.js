4c8ba1aabe02c7b149b14c8afca7bdb9567e7351
Felix Wei <felixwei7@gmail.com>
2023-02-23 12:43:21 -0800

    restructured folders like 1537

---
+++ CASA/app/data/lists.js
@@ -0,0 +1,5 @@
+let daysOfTheWeekEnglish = "<ul><li>Monday</li><li>Tuesday</li><li>Wednesday</li><li>Thursday</li><li>Friday</li><li>Saturday</li><li>Sunday</li></ul>";
+
+let daysOfTheWeekFrench = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+
