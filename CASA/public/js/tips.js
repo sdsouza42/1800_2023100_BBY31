a6e4aa326605a85a8161629b11ff646bcc7bd8a9
Felix Wei <felixwei7@gmail.com>
2023-03-09 10:45:59 -0800

    made tips populate from firebase

--- CASA/public/js/tips.js
+++ CASA/public/js/tips.js
@@ -1,4 +1,25 @@
 // populate page with the relevant tip from tips collections
+function displayTip() {
+  // let params = new URL( window.location.href ); //get URL of search bar
+  // let ID = params.searchParams.get( "docID" ); //get value for key "id"
+  // console.log( ID );
+
+  db.collection( "Tips" )
+      .doc( "QZft0ZW41tZLyWdp6Zr0" )
+      .get()
+      .then( doc => {
+          weatherForThisTip = doc.data().weather;
+          tipForThisWeather = doc.data().info;
+          
+          // only populate title, and image
+          document.getElementById( "weatherForThisTip" ).innerHTML = weatherForThisTip;
+          document.getElementById( "tipForThisWeather" ).innerHTML = tipForThisWeather;
+      } );
+      console.log("displayTip ran properly");
+}
+displayTip();
+
+
 
 
 // save button adds this tip to the user-specific save list
