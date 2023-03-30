95023a2f72a7a7bb57a4f2a59b6c00601e43432c
Felix Wei <felixwei7@gmail.com>
2023-03-30 13:03:48 -0700

    working on bookmarks

--- CASA/public/js/savelist.js
+++ CASA/public/js/savelist.js
@@ -1,41 +1,60 @@
-// populate page with multiple tips from the user-specific savelist 
-function populateTips() {
-    db.collection("user").where("savedTips").get()
+//Global variable pointing to the current user's Firestore document
+var currentUser;   
+var bookmarks;
+var saveListPlaceholderReplacement = "";
 
-    console.log(db.collection("user").where("savedTips").get());
-    
-    let hikeCardTemplate = document.getElementById("reviewCardTemplate");
-    let hikeCardGroup = document.getElementById("reviewCardGroup");
+//Function that calls everything needed for the main page  
+function doAll() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("user").doc(user.uid); //global
+            // console.log("user.uid: " + user.uid);
 
-    //let params = new URL(window.location.href) //get the url from the searbar
-    //let hikeID = params.searchParams.get("docID");
-    var hikeID = localStorage.getItem("hikeDocID");
-    
-    // doublecheck: is your collection called "Reviews" or "reviews"?
-    db.collection("reviews").where( "hikeDocID", "==", hikeID).get()
-        .then(allReviews => {
-            reviews=allReviews.docs;
-            console.log(reviews);
-            reviews.forEach(doc => {
-                var title = doc.data().title; //gets the name field
-                var level = doc.data().level; //gets the unique ID field
-                var season = doc.data().season;
-                var description = doc.data().description; //gets the length field
-                var flooded = doc.data().flooded;
-                var scrambled = doc.data().scrambled;
-                var time = doc.data().timestamp.toDate();
-                console.log(time)
 
-                let reviewCard = hikeCardTemplate.content.cloneNode(true);
-                reviewCard.querySelector('.title').innerHTML = title;     //equiv getElementByClassName
-                reviewCard.querySelector('.time').innerHTML = new Date(time).toLocaleString();;     //equiv getElementByClassName
-                reviewCard.querySelector('.level').innerHTML = `level: ${level}`;
-                reviewCard.querySelector('.season').innerHTML = `season: ${season}`;
-                reviewCard.querySelector('.scrambled').innerHTML = `scrambled: ${scrambled}`;  //equiv getElementByClassName
-                reviewCard.querySelector('.flooded').innerHTML = `flooded: ${flooded}`;  //equiv getElementByClassName
-                reviewCard.querySelector('.description').innerHTML = `Description: ${description}`;
-                hikeCardGroup.appendChild(reviewCard);
-            })
-        })
+            insertSavedTips();
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+            window.location.href = "/html/login.html";
+        }
+    });
 }
-populateTips();
+doAll();
+
+// Get savedTips
+function insertSavedTips() {
+    currentUser.get().then(userDoc => {
+        //get the user name
+        var bookmarks = userDoc.data().bookmarks;
+
+        for (let i = 0; i < bookmarks.length; i++) {
+            console.log(bookmarks[i]);
+
+
+            saveListPlaceholderReplacement += 
+            "<button onclick=\""+bookmarks[i]+"\"()"+">"+bookmarks[i]+"</button>" +
+            "<script>" +
+                "function "+bookmarks[i]+"() {" +
+                  "const firebaseTipID = \""+bookmarks[i]+"\";" +
+                  "localStorage.setItem('firebaseTipID', firebaseTipID); // stores ID for the tip linked to this alert in localStorage" +
+                  "window.location.href = "+"/html/tips.html"+"; // redirect to the tips page" +
+                "}" +
+            "</script>";
+
+        }
+
+        console.log("saveListPlaceholderReplacement: " + saveListPlaceholderReplacement);
+
+        // replace saveListPlaceholder
+        document.getElementById("saveListPlaceholder").innerHTML = saveListPlaceholderReplacement; 
+      
+
+
+
+        // $("#name-goes-here").text(user_Name); //jquery
+        // document.getElementByID("name-goes-here").innetText=user_Name;
+    })
+}
+
+
+
--- CASA/public/js/tips copy.js
+++ CASA/public/js/tips copy.js
@@ -1,6 +1,3 @@
-//Global variable pointing to the current user's Firestore document
-var currentUser;   
-
 // get the tip ID previously stored from the corresponding alert
 const firebaseTipID = localStorage.getItem('firebaseTipID');
 console.log("Tip has loaded from: " + firebaseTipID);
@@ -18,13 +15,10 @@ function displayTip() {
           $("#weatherForThisTip").html(weatherForThisTip);
           $("#tipForThisWeather").html(tipForThisWeather);
       } );
-
-      doc.querySelector('i').onclick = () => saveBookmark(docID);
 }
 displayTip();
 
-
-//Function that calls everything needed for the main page  
+// to remember user
 function doAll() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
@@ -32,40 +26,52 @@ function doAll() {
             console.log("user.uid: " + user.uid);
 
             insertNameFromFirestore();
-            // displayCardsDynamically("hikes");
         } else {
             // No user is signed in.
             console.log("No user is signed in");
-            window.location.href = "/html/login.html";
+            window.location.href = "login.html";
         }
-    });
+    }); 
+
+    
 }
 doAll();
 
-// Get savedTips
+// Insert name function using the global variable "currentUser"
 function insertNameFromFirestore() {
     currentUser.get().then(userDoc => {
         //get the user name
-        var bookmarks = userDoc.data().bookmarks;
-        console.log("bookmarks: " + bookmarks);
-        // $("#name-goes-here").text(user_Name); //jquery
+        var user_Name = userDoc.data().name;
+        console.log(user_Name);
+        $("#name-goes-here").text(user_Name); //jquery
         // document.getElementByID("name-goes-here").innetText=user_Name;
     })
 }
-// Comment out the next line (we will call this function from doAll())
-// insertNameFromFirestore();
 
-function saveBookmark(hikeDocID) {
+
+// function to save a tip to the user's personal savelist
+$("#tipSaveInput").onclick = () => saveBookmark(firebaseTipID);
+// newcard.querySelector('i').id = 'save-' + docID; 
+
+function saveBookmark(firebaseTipID) {
+    console.log("save button clicked");
     currentUser.set({
-            bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeDocID)
+            bookmarks: firebase.firestore.FieldValue.arrayUnion(firebaseTipID)
         }, {
             merge: true
         })
         .then(function () {
-            console.log("bookmark has been saved for: " + currentUser);
-            var iconID = 'save-' + hikeDocID;
-            //console.log(iconID);
-						//this is to change the icon of the hike that was saved to "filled"
-            document.getElementById(iconID).innerText = 'bookmark';
+            console.log("tip has been saved for: " + currentUser);
+            // var iconID = 'save-' + hikeDocID;
+            //this is to change the icon of the hike that was saved to "filled"
+            // document.getElementById(tipSaveInput).innerText = 'bookmark';
         });
 }
+
+
+
+
+// function to remove this tip if the savelist if already there
+
+
+// change button displayer depending on whether the tip is already saved or not
