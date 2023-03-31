d39cbe36bcd31821c742780c4a0e59f8a4ea7def
Eddie Xu <wxu55@my.bcit.ca>
2023-03-31 15:33:57 -0700

    NavtopPic store Profile Pic

--- CASA/public/js/skeleton.js
+++ CASA/public/js/skeleton.js
@@ -1,20 +1,27 @@
-$(document).ready(function() {
-  // inserts top NavBar  
+$(document).ready(function () {
+  // inserts top NavBar
   $.ajax({
-      url: '/html/navTop.html',
-      success: function(data) {
-        $('#navTopPlaceholder').html(data);
-        // replace page title with meta name value
-        var pageTitle = $('meta').attr('name');
-        $('#pageTitle').text(pageTitle);
+    url: "/html/navTop.html",
+    success: function (data) {
+      $("#navTopPlaceholder").html(data);
+      // replace page title with meta name value
+      var pageTitle = $("meta").attr("name");
+      $("#pageTitle").text(pageTitle);
+
+      // Update the navigation bar profile picture if available in localStorage
+      var storedNavProfilePic = localStorage.getItem("navProfilePic");
+      if (storedNavProfilePic) {
+        const navProfilePic = document.getElementById("navProfilePic");
+        navProfilePic.src = storedNavProfilePic;
       }
-    });
+    },
+  });
   // inserts bottom NavBar
   $.ajax({
-      url: '/html/navBottom.html',
-      success: function(data) {
-        $('#navBottomPlaceholder').html(data);
-      }
-    });      
+    url: "/html/navBottom.html",
+    success: function (data) {
+      $("#navBottomPlaceholder").html(data);
+    },
   });
+});
 
