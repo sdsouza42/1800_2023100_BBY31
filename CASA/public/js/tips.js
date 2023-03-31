2a871e3b1bef197df5a998056900f939c322fb02
Felix Wei <felixwei7@gmail.com>
2023-03-31 13:20:07 -0700

    bookmarking working now

--- CASA/public/js/tips.js
+++ CASA/public/js/tips.js
@@ -3,11 +3,10 @@ var currentUser;
 
 // get the tip ID previously stored from the corresponding alert
 const firebaseTipID = localStorage.getItem('firebaseTipID');
-console.log("Tip has loaded from: " + firebaseTipID);
+// console.log("Tip has loaded from: " + firebaseTipID);
 
 // populate page with the relevant tip from tips collections
 function displayTip() {
-
   db.collection( "Tips" )
       .doc( firebaseTipID )
       .get()
@@ -22,30 +21,27 @@ function displayTip() {
       // add event listener to bookmark icon
       document.getElementById("saveTipButtonIcon").onclick = () => saveBookmark(firebaseTipID);
 
-      // make bookmark icon appear black if already saved
-      currentUser.get().then(userDoc => {
-        //get the user name
-        var bookmarks = userDoc.data().bookmarks;
-        if (bookmarks.includes(firebaseTipID)) {
-           document.getElementById('save-' + docID).innerText = 'bookmark';
-        }
-  })
+      // make bookmark icon appear solid fill if already saved
+      if (currentUser) {
+        currentUser.get().then(userDoc => {
+            //get the user name
+            var bookmarks = userDoc.data().bookmarks;
+            console.log("truth: " + bookmarks.includes(firebaseTipID));
+            if (bookmarks.includes(firebaseTipID)) {
+                document.getElementById("saveTipButtonIcon").innerText = 'bookmark';
+            }
+        })
+    }
 }
-displayTip();
-
-console.log(firebase.auth()); // Check if firebase.auth() returns a valid object
-console.log(db.collection("user")); // Check if db.collection("user") returns a valid object
 
 // Get the user ID and database
 function doAll() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
             currentUser = db.collection("user").doc(user.uid); //global
-            console.log(currentUser);
-            console.log("user.uid: " + user.uid);
-
-            insertNameFromFirestore();
-            // displayCardsDynamically("hikes");
+            // console.log(currentUser);
+            // console.log("user.uid: " + user.uid);
+            displayTip(); // display tip after get the user details
         } else {
             // No user is signed in.
             console.log("No user is signed in");
@@ -63,10 +59,11 @@ function saveBookmark(firebaseTipID) {
             merge: true
         })
         .then(function () {
-            console.log("bookmark has been saved for: " + currentUser);
-            var iconID = 'save-' + hikeDocID;
+            console.log("bookmark has been saved for: " + user.uid);
             //console.log(iconID);
 			//this is to change the icon of the hike that was saved to "filled"
-            document.getElementById(iconID).innerText = 'bookmark';
+            document.getElementById("saveTipButtonIcon").innerText = 'bookmark';
         });
 }
+
+// unsave tip
