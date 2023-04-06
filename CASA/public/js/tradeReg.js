41603e03c64a1a1fb639dc7d6fd8798fba6c9f6b
sdsouza42 <dsouza.savio@hotmail.com>
2023-04-06 08:03:06 -0700

    changes

--- CASA/public/js/tradeReg.js
+++ CASA/public/js/tradeReg.js
@@ -4,10 +4,10 @@ registrationForm.addEventListener("submit", async (e) => {
 
   const bIDNum = parseInt(document.getElementById("bIDNum").value) || null;
   const bName = document.getElementById("bName").value;
-  const nNum = parseInt(document.getElementById("nNum").value);
+  const bNum = parseInt(document.getElementById("nNum").value);
   const distance = document.getElementById("distance").value;
   const email = document.getElementById("email").value;
-  const name = document.getElementById("name").value;
+  const Name = document.getElementById("name").value;
   // const tradePicture = document.getElementById("tradePicture").files[0];
   const tradeTags = Array.from(document.querySelectorAll('input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
 
@@ -28,6 +28,9 @@ registrationForm.addEventListener("submit", async (e) => {
     });
 
     console.log("Document written with ID: ", docRef.id);
+    setTimeout(() => {
+      window.location.href = "./TradeSelect.html"; 
+    }, 2000);
   } catch (error) {
     console.error("Error adding document: ", error);
   }
